520a8cdadbcd488b8970b36f8eb0381c68fb3802
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2021-12-09 01:49:07 +0100

    Update sha

--- level3.js
+++ level3.js
@@ -80,21 +80,14 @@ console.log(`converting RGB to HEX`,convertRgbToHex('125,244,255'));
 // Write a function generateColors which can generate any number of hexa or rgb colors.
 
 const generateColors = (type, number) => {
-  switch (type) {
-    case `hexa` : 
-      arrayOfHexaColors(number)
-      break;
-    
-    case `rgb` :
-      arrayOfRgbColors(number)
-      break;
-    default:
-     'Please enter valid type'; 
-      break;
+
+  if (type === `hexa`) {
+    return arrayOfHexaColors(number);
   }
-  // type ===  || type === || type === || type === `HEXA` ? 
-  //   : type ===  || type === `RGB` ? 
-  //   : ;
+  else if (type === `rgb`) {
+    return arrayOfRgbColors(number);
+  }
+else {return `type is neither hexadecimal or rgb spectrum`}
 }
 console.log(`generate colors`, generateColors('hexa', 5));
 
@@ -105,25 +98,112 @@ console.log(generateColors('rgb', 1)) // 'rgb(33,79, 176)'
 
 // Call your function shuffleArray, it takes an array as a parameter and it returns a shuffled array
 
+
 // Call your function factorial, it takes a whole number as a parameter and it return a factorial of the number
 
+const factorial = (num) => {
+val = 1;
+for(i=1; i<=num; i++){
+val *=i;
+
+}
+return val;
+}
+
+console.log(factorial(10));
+
+// ALTERNATIVE
+
+function factorialize(num) {
+  for (var product = 1; num > 0; num--) {
+    product *= num;
+  }
+  return product;
+}
+
+console.log(factorialize(10));
+
+
 // Call your function isEmpty, it takes a parameter and it checks if it is empty or not
+const isEmpty = (param) => {
+  if (param === undefined) {
+    return `The parameter is empty`
+  }
+  else {
+    return `The parameter is not empty`;
+  }
+}
+console.log(isEmpty())
+console.log(isEmpty(2))
+
 
 // Call your function sum, it takes any number of arguments and it returns the sum.
+const sum = (...args) => {
+  let finalVal = 0;
+  for (i = 0; i < args.length; i++){
+    finalVal += args[i];
+  }
+  return finalVal;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
 
 // Write a function called sumOfArrayItems, it takes an array parameter and return the sum of all the items. Check if all the array items are number types. If not give return reasonable feedback.
+const sumOfArrayItems = (args) => {
+  let finalVal = 0;
+  
+  for (i = 0; i < args.length; i++) {
+    if (typeof (args[i]) === `number`) {
+      finalVal += args[i];
+    } else {
+      return `NO or NOT ALL INTEGER VALUES`
+    }
+  }
+  return finalVal;
+};
+console.log(sumOfArrayItems([2, 4, 3]));
 
 // Write a function called average, it takes an array parameter and returns the average of the items. Check if all the array items are number types. If not give return reasonable feedback.
+const average = (param) => {
+  let sum = 0;
+  param = param.map(num => sum += num);
+  return sum / param.length;
+
+}
+console.log(average([2,4,6,8]))
 
 // Write a function called modifyArray takes array as parameter and modifies the fifth item of the array and return the array. If the array length is less than five it return 'item not found'.
-
-// console.log(modifyArray(['Avocado', 'Tomato', 'Potato','Mango', 'Lemon','Carrot']);
+const modifyArray = (arr) => {
+  if (arr.length >= 5) {
+    arr[4] = arr[4].toUpperCase();
+    return arr;
+  }
+  else {
+    return `Item not found`
+  };
+}
+console.log(modifyArray(['Avocado', 'Tomato', 'Potato','Mango', 'Lemon','Carrot']));
 // ['Avocado', 'Tomato', 'Potato','Mango', 'LEMON', 'Carrot']
-// console.log(modifyArray(['Google', 'Facebook','Apple', 'Amazon','Microsoft',  'IBM']);
+console.log(modifyArray(['Google', 'Facebook','Apple', 'Amazon','Microsoft',  'IBM']));
 // ['Google', 'Facebook','Apple', 'Amazon','MICROSOFT',  'IBM']
-// console.log(modifyArray(['Google', 'Facebook','Apple', 'Amazon']);
+console.log(modifyArray(['Google', 'Facebook','Apple', 'Amazon']));
 //   'Not Found'
+
+
 // Write a function called isPrime, which checks if a number is prime number.
+function isPrime(num) {
+  if (num < 2) { return false };
+
+  
+  for (let k = 2; k < num; k++) {
+    if (num % k == 0) {
+      return `${num} isn't a prime number`;
+    }
+  }
+  return `${num} is a prime number`;
+}
+console.log(isPrime(5));
+console.log(isPrime(4));
 
 // Write a functions which checks if all items are unique in the array.
 
@@ -171,3 +251,25 @@ getPersonInfo(person);
 /*
 Asabeneh Yetayeh lives in Finland. He is  250 years old. He is an Instructor and Developer. He teaches HTML, CSS, JavaScript, React, Redux, Node, MongoDB, Python and D3.js. He speaks Amharic, English and a little bit of Suomi(Finnish)
 */
+
+function anagram(a, b) {   
+  if (a.length === b.length) {
+     const aSort = a.split('').sort().join();
+    const bSort = b.split('').sort().join();
+      
+    if (aSort === bSort) {
+      return(`${a} and ${b} are anagrams to each other`)
+    }
+    else {
+      return `${a} and ${b} are not anagram to each other`;
+    }
+
+      }
+  else {
+    return `${a} and ${b} are not of equal length`
+    }
+};
+  
+console.log(anagram(`silent`,`listener`))
+
+
